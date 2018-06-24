1a935f122845064cce7d7ce427009053721ba127
EleazarSauz <eleazar48a@gmail.com>
2018-06-24 05:06:14 -0500

    nadvar

--- ui/src/components/landing/Lan.js
+++ ui/src/components/landing/Lan.js
@@ -22,13 +22,59 @@ class Lan extends Component {
   }
 
   render() {
-    return (
-      <div className="App">
-      <button onClick={this.handleAuth} className="App-btn">
-        Iniciar sesión
-      </button>
-      </div>
-    );
+    return <div className="App">
+        <nav class="navbar navbar-expand-lg fixed-top navbar-shrink" id="mainNav">
+          <div class="container">
+            <a class="navbar-brand js-scroll-trigger" href="http://themes.startbootstrap.com/vitality-v2.2.0/video.html#page-top">
+              <img class="img-fluid" src="./Vitality - Multipurpose Bootstrap 4 One Page Theme_files/logo.svg" alt="" />
+            </a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+              Menu
+              <i class="fa fa-bars" />
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+              <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                  <a class="nav-link js-scroll-trigger" href="#about">
+                    About
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link js-scroll-trigger" href="#team">
+                    Team
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link js-scroll-trigger" href="#services">
+                    Process
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link js-scroll-trigger" href="#work">
+                    Work
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link js-scroll-trigger" href="#pricing">
+                    Pricing
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link js-scroll-trigger active" href="#contact">
+                    Contact
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <button onClick={this.handleAuth} className="App-btn">
+                    Iniciar sesión
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        
+      </div>;
   }
 }
 
